bf5adcc98f058d6773e1a6adec3f58b1495948fb
Brendan Edwards <brendan.edwards141@gmail.com>
2022-01-24 11:28:29 -0600

    epdating exercises

--- Misc./warm-up-01.js
+++ Misc./warm-up-01.js
@@ -1,7 +1,9 @@
-var bugs = ["mosquito", "ant", "scorpion", "ant", "ant", "mosquito", "typo", "reference error", "type error"];
+// var bugs = ["mosquito", "ant", "scorpion", "ant", "ant", "mosquito", "typo", "reference error", "type error"];
 
 
 
 // removeAll(bugs, "ant") should return ["mosquito", "scorpion", "mosquito", "typo", "reference error", "type error"]
 // removeAll(bugs, "mosquito") should return ["ant", "scorpion", "ant", "ant", "typo", "reference error", "type error"]
 //removeAll(bugs, "roach") should return the original array b/c “roach” has no occurrences.
+
+function reverseString('goodbye')
