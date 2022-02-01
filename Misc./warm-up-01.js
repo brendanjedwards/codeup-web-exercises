afe52be9e05f50314c3980b0e07e46783a5a3cb0
Brendan Edwards <brendan.edwards141@gmail.com>
2022-02-01 12:01:42 -0600

    box model updat

--- Misc./warm-up-01.js
+++ Misc./warm-up-01.js
@@ -6,4 +6,12 @@
 // removeAll(bugs, "mosquito") should return ["ant", "scorpion", "ant", "ant", "typo", "reference error", "type error"]
 //removeAll(bugs, "roach") should return the original array b/c “roach” has no occurrences.
 
-function reverseString('goodbye')
+//function reverseString('goodbye')
+
+console.log(whichStringIsLonger("bobby", "lou")); // returns first
+console.log(whichStringIsLonger("bob", "louise")); // returns second
+console.log(whichStringIsLonger("bob", "lou")); // returns neither
+console.log(whichStringIsLonger("bobby", 2)); // returns false
+console.log(whichStringIsLonger(1, "lou")); // returns false
+
+whichStringIsLonger();
