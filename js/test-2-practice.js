39acd846f5ebb642ce814edfe783d59cbc593b48
Brendan Edwards <brendan.edwards141@gmail.com>
2022-02-04 15:14:50 -0600

    uodate push

---
+++ js/test-2-practice.js
@@ -0,0 +1,14 @@
+// isANumber
+// function isANumber
+//
+// isANumber(23)               // true
+//
+// typeof '42'
+
+function isANumber(aNumber){
+    aNumber(42
+// console.log(typeof 42)
+// return false
+}
+//
+console.log(isANumber(42))
